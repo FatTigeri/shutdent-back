05a2b9318c29662ee6488ca306c4241b4d2a6156
Eason <1501919885@qq.com>
2022-09-05 19:17:18 +0800

    init初始化

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入axois用来进行请求数据的发送
+import axois from 'axois'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axois
 
 new Vue({
   router,
